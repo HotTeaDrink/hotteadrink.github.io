c0b5e40e85f64eeaf9467aee8aa7297bfe4fd081
HotTeaDrink <xxtopgear69xx@gmail.com>
2025-01-30 18:37:59 +0100

    still trying to fix...

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'hotteadrink.github.io',
   plugins: [react({
     babel: {
       plugins: [['babel-plugin-react-compiler', { target: '18' }]],
