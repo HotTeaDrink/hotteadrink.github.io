efbc9285aecdbcec054057e03638fba7279924b7
HotTeaDrink <xxtopgear69xx@gmail.com>
2025-01-30 18:43:53 +0100

    try v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/hotteadrink.github.io/',
-  plugins: [react({
-    babel: {
-      plugins: [['babel-plugin-react-compiler', { target: '18' }]],
-    },
-  })
+  plugins: [react()
 ],
   optimizeDeps: {
     include: ['lucide-react','react', 'react-dom'],
