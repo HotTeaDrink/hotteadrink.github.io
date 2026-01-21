95dfbde5d6d4b912f8c3205d0ab505d97bbbb826
HotTeaDrink <75931449+HotTeaDrink@users.noreply.github.com>
2026-01-21 20:51:58 +0100

    added pdf pricing

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import image3 from "./public/assets/images/carrousel/image3.jpg";
 import image4 from "./public/assets/images/carrousel/image4.png";
 import image5 from "./public/assets/images/carrousel/image5.png";
 
+import tarif_pdf from './public/assets/tarif_2025-2026.pdf';
+
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -106,14 +108,14 @@ function App() {
                 <span className="ml-2 text-xl font-semibold">DentalDesign</span>
               </div>
             </a>
-            
+
             <div className="hidden md:flex space-x-8">
               <a href="#services" className="text-gray-700 hover:text-blue-600">Nos Services</a>
               <a href="#about" className="text-gray-700 hover:text-blue-600">À propos</a>
               <a href="#contact" className="text-gray-700 hover:text-blue-600">Nos Contact</a>
             </div>
 
-            <button 
+            <button
               className="md:hidden"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
@@ -145,8 +147,8 @@ function App() {
           <img className="h-64 w-64 mx-auto mb-6 text-blue-400" src={logo} />
           <h1 className="text-4xl md:text-6xl font-bold mb-4">DentalDesign</h1>
           <p className="text-xl md:text-2xl max-w-2xl mx-auto">
-          Prothésiste dentaire aguerri, expert en adjointe numérique pour châssis, je sublime vos prothèses avec des
-          conceptions d'une qualité exceptionnelle.
+            Prothésiste dentaire aguerri, expert en adjointe numérique pour châssis, je sublime vos prothèses avec des
+            conceptions d'une qualité exceptionnelle.
           </p>
         </div>
       </section>
@@ -156,7 +158,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid md:grid-cols-2 gap-12 items-center">
             <div className="relative h-[400px]">
-              <img 
+              <img
                 src={imageService}
                 alt="Services"
                 className="w-full h-full object-cover rounded-lg shadow-lg"
@@ -165,7 +167,7 @@ function App() {
             <div>
               <h2 className="text-3xl font-bold mb-6">Nos Services</h2>
               <p className="text-gray-600 mb-6">
-              Des conceptions de châssis, à la fois rapides et abordables. Bénéficiez d'une efficacité redoutable et de tarifs ultra-compétitifs pour des prothèses dentaires de qualité. L'esthétique et la fonctionnalité sont des éléments-clés dans la conception de châssis. Nous veillerons à ce que votre design soit à la fois attrayant et conforme aux normes de fabrication requit par les centres de production. 
+                Des conceptions de châssis, à la fois rapides et abordables. Bénéficiez d'une efficacité redoutable et de tarifs ultra-compétitifs pour des prothèses dentaires de qualité. L'esthétique et la fonctionnalité sont des éléments-clés dans la conception de châssis. Nous veillerons à ce que votre design soit à la fois attrayant et conforme aux normes de fabrication requit par les centres de production.
               </p>
               <ul className="grid grid-cols-2 gap-x-8 gap-y-4 mb-8">
                 <li className="flex items-center">
@@ -204,19 +206,14 @@ function App() {
       <section className="bg-gray-50 py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-center mb-12">Nos Tarifs</h2>
-          <div className="grid md:grid-cols-2 gap-8">
-            {[
-              { name: 'Design châssis', price: '25€' },
-              { name: 'Design modèle', price: '4€' }
-            ].map((item, index) => (
-              <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
-                <h3 className="text-xl font-semibold mb-4">{item.name}</h3>
-                <div className="flex items-center justify-between">
-                    <p className="text-3xl font-bold text-blue-600 mr-2">{item.price}</p>
-                    <span className="text-m text-red-600 ml-auto">{item.name === "Design châssis" && "*7€ par CP ou dent massive"}</span>
-                </div>
+          <div className="max-w-5xl mx-auto">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <iframe
+                src={tarif_pdf}
+                className="w-full h-[600px] md:h-[800px]"
+                title="Tarifs DentalDesign"
+              />
             </div>
-            ))}
           </div>
         </div>
       </section>
@@ -260,7 +257,7 @@ function App() {
                 Notre expertise en conception de châssis est inégalée.
               </p>
               <p className="text-gray-600 mb-6">
-              Forts de notre expérience et de notre passion, nous vous invitons à intégrer notre design révolutionnaire dans le flux numérique de votre laboratoire. Bénéficiez d'une transition fluide vers des solutions modernes et efficaces, alliant technologie de pointe et service sur mesure. Collaborons ensemble pour des résultats optimaux et des sourires éclatants !
+                Forts de notre expérience et de notre passion, nous vous invitons à intégrer notre design révolutionnaire dans le flux numérique de votre laboratoire. Bénéficiez d'une transition fluide vers des solutions modernes et efficaces, alliant technologie de pointe et service sur mesure. Collaborons ensemble pour des résultats optimaux et des sourires éclatants !
               </p>
               <p className="text-gray-600 mb-8">
                 Contactez-nous dès aujourd'hui pour bénéficier de notre expertise et transformer vos projets en réalité. Nous sommes impatients de collaborer avec vous et de contribuer à votre succès !
@@ -276,10 +273,10 @@ function App() {
             <div className="relative h-[400px] bg-gray-100 rounded-lg overflow-hidden">
               <div className="absolute inset-0 flex items-center justify-center">
                 <div className="relative w-full h-full">
-                <video controls loop autoPlay>
-                  <source src={videoSource} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                  <video controls loop autoPlay>
+                    <source src={videoSource} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
                 </div>
               </div>
             </div>
@@ -295,10 +292,10 @@ function App() {
             <h2 className="text-3xl font-bold mb-6">Service client</h2>
             <p className="text-gray-600 text-lg mb-8">
               Chez Dental design, notre priorité est de vous offrir une expérience cliente exceptionnelle !
-              Nous sommes là pour vous écouter, répondre à toutes vos questions et vous accompagner à chaque étape de votre projet. 
+              Nous sommes là pour vous écouter, répondre à toutes vos questions et vous accompagner à chaque étape de votre projet.
             </p>
             <p className="text-gray-600 text-lg mb-8">
-              Notre équipe est dévouée à votre entière satisfaction et fera tout son possible pour dépasser vos attentes. 
+              Notre équipe est dévouée à votre entière satisfaction et fera tout son possible pour dépasser vos attentes.
             </p>
             <a href='#contact'>
               <button className="bg-blue-600 text-white px-8 py-3 rounded-lg hover:bg-blue-700 transition">
@@ -311,37 +308,36 @@ function App() {
 
       {/* Gallery Section */}
       <section className="bg-gray-50 py-20">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center mb-12">Galerie</h2>
-        <div className="relative">
-          <div className="relative w-full h-[500px] overflow-hidden rounded-lg">
-            <div
-              className={`absolute w-full h-full transition-opacity duration-500 ease-in-out ${
-                isTransitioning ? "opacity-0" : "opacity-100"
-              }`}
-            >
-              <img
-                src={galleryImages[currentSlide]}
-                alt={`Slide ${currentSlide + 1}`}
-                className="w-full h-full object-cover rounded-lg"
-              />
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-3xl font-bold text-center mb-12">Galerie</h2>
+          <div className="relative">
+            <div className="relative w-full h-[500px] overflow-hidden rounded-lg">
+              <div
+                className={`absolute w-full h-full transition-opacity duration-500 ease-in-out ${isTransitioning ? "opacity-0" : "opacity-100"
+                  }`}
+              >
+                <img
+                  src={galleryImages[currentSlide]}
+                  alt={`Slide ${currentSlide + 1}`}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
             </div>
+            <button
+              onClick={prevSlide}
+              className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full"
+            >
+              <ChevronLeft className="h-6 w-6" />
+            </button>
+            <button
+              onClick={nextSlide}
+              className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full"
+            >
+              <ChevronRight className="h-6 w-6" />
+            </button>
           </div>
-          <button
-            onClick={prevSlide}
-            className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full"
-          >
-            <ChevronLeft className="h-6 w-6" />
-          </button>
-          <button
-            onClick={nextSlide}
-            className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 p-2 rounded-full"
-          >
-            <ChevronRight className="h-6 w-6" />
-          </button>
         </div>
-      </div>
-    </section>
+      </section>
 
       {/* Contact Section */}
       <section id="contact" className="py-20">
